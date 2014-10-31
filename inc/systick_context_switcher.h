58dc5712fb85dd2d9909297147f3cf82f97ddee5
Jeremy Hunt <jrh6@rice.edu>
2014-10-31 16:33:51 -0500

    Added systick init and a stack setup for the threads.

--- inc/systick_context_switcher.h
+++ inc/systick_context_switcher.h
@@ -9,12 +9,14 @@
 
 #include <sys/types.h>
 
-/* 10ms Scheduler Ticks */
-#define HIGH_PRIO_TICK_FREQ		100
-#define LOW_PRIO_TICK_FREQ		2
-#define APB1_FREQ				36000000
-#define APB2_FREQ				36000000
+#define AHB_FREQ				72000000
+#define APB1_FREQ				(AHB_FREQ/2)
+#define APB2_FREQ				(AHB_FREQ/2)
 
+/* 1ms SysTick Interrupt */
+#define SYSTICK_IN_FREQ			(AHB_FREQ/8)
+#define SYSTICK_FREQ			1000
+#define SYSTICK_COUNT			(SYSTICK_IN_FREQ/SYSTICK_FREQ)
 
 /* Create a quick boolean type */
 typedef int boolean;
@@ -22,6 +24,26 @@ typedef int boolean;
 #define FALSE	0
 
 /* Define the exception stack frame */
+#define ESF_LEN			32
+#define ESF_OFFSET_R0	0x00
+#define ESF_OFFSET_R1	0x04
+#define ESF_OFFSET_R2	0x08
+#define ESF_OFFSET_R3	0x0C
+#define ESF_OFFSET_R12	0x10
+#define ESF_OFFSET_LR	0x14
+#define ESF_OFFSET_PC	0x18
+#define ESF_OFFSET_xPSR	0x1C
+
+#define SSF_LEN			32
+#define SSF_OFFSET_R4	0x00
+#define SSF_OFFSET_R5	0x04
+#define SSF_OFFSET_R6	0x08
+#define SSF_OFFSET_R7	0x0C
+#define SSF_OFFSET_R8	0x10
+#define SSF_OFFSET_R9	0x14
+#define SSF_OFFSET_R10	0x18
+#define SSF_OFFSET_R11	0x1C
+
 typedef struct exception_stack_frame_s {
 	uint32_t R0;
 	uint32_t R1;
@@ -32,5 +54,7 @@ typedef struct exception_stack_frame_s {
 	uint32_t PC;
 	uint32_t xPSR;
 } __attribute__((packed, aligned(4))) exception_stack_frame_t;
+
+/*  */
 
 #endif /* SYSTICK_CONTEXT_SWITCHER_H_ */
--- src/systick_context_switcher.c
+++ src/systick_context_switcher.c
@@ -17,8 +17,26 @@
 #include <stddef.h>
 #include <sys/types.h>
 
-/* This is the second stack */
-uint8_t stack_a[2048];
+/* These are the other stacks */
+#define STACK_LEN	2048
+uint8_t stack_a[STACK_LEN];
+uint8_t stack_b[STACK_LEN];
+
+/* The thread return place in inline assembly */
+extern void ThreadExitReturn();
+
+/*
+ * Initialize the SysTick interrupt for 1ms.
+ */
+void systick_init() {
+	/* Set up the SysTick interrupt  */
+	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);
+	/* This sets the time-base and starts the interrupt */
+	if (SysTick_Config(SYSTICK_COUNT)) {
+		/* We failed. Stop. */
+		for (;;) {}
+	}
+}
 
 /*
  * Initialize the GPIO which controls the LED
@@ -35,6 +53,9 @@ static void leds_init() {
 	GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
 }
 
+/*
+ * Blink the light without interrupts to test a basic thread.
+ */
 int blink_light() {
 	unsigned int greenLedState = 0;
 	volatile int i;
@@ -57,6 +78,42 @@ int blink_light() {
 }
 
 /*
+ * Initialize a thread's stack so that when it is context switched to it will start the given
+ * function with the correct state. This also sets up the pc so that when the thread exits,
+ * it jumps to the ThreadExitReturn, which switches to the main control loop taking
+ * return values and cleaning up the thread.
+ */
+void stack_thread_init(int (*thread)(void), void* stack_top) {
+	/* Figure out where the exception and software stack frames should be on the stack */
+	uint32_t* esf = (uint32_t*)((uint8_t*)stack_top - ESF_LEN);
+	uint32_t* ssf = (uint32_t*)((uint8_t*)stack_top - ESF_LEN - SSF_LEN);
+
+	/* Artificially push the information that would normally be pushed by hardware. */
+	/* Registers are all 0 */
+	esf[ESF_OFFSET_R0] = 0;
+	esf[ESF_OFFSET_R1] = 0;
+	esf[ESF_OFFSET_R2] = 0;
+	esf[ESF_OFFSET_R3] = 0;
+	esf[ESF_OFFSET_R12] = 0;
+	/* The thread should return to ThreadExitReturn on exit */
+	esf[ESF_OFFSET_LR] = (uint32_t)(&ThreadExitReturn);
+	/* The thread should start at the given handler */
+	esf[ESF_OFFSET_PC] = (uint32_t)(thread);
+	/* This is the reset value of the PSR  TODO: Should this be something else? */
+	esf[ESF_OFFSET_xPSR] = 0x01000000;
+
+	/* Now push the info for the software (i.e. the other registers) */
+	ssf[SSF_OFFSET_R4] = 0;
+	ssf[SSF_OFFSET_R5] = 0;
+	ssf[SSF_OFFSET_R6] = 0;
+	ssf[SSF_OFFSET_R7] = 0;
+	ssf[SSF_OFFSET_R8] = 0;
+	ssf[SSF_OFFSET_R9] = 0;
+	ssf[SSF_OFFSET_R10] = 0;
+	ssf[SSF_OFFSET_R11] = 0;
+}
+
+/*
  * Switch the stack to the PSP from the MSP and execute the given function.
  */
 int stack_switch(int (*thread)(void), void* new_stack_top) {
@@ -73,9 +130,9 @@ int stack_switch(int (*thread)(void), void* new_stack_top) {
     		"ORR r0, r0, #0x2\n\t"
     		"MSR control, r0\n\t"
 			/* Call the function. This sets PC and LR. The function sets R0. */
-			"BLX %[funcptr]\n\t"
+			"BLX %[funcptr]\n"
 			/* When thread ends, it should return here. Switch back the stack. */
-    		"MRS r1, control\n\t"
+    		"ThreadExitReturn:	MRS r1, control\n\t"
     		"BIC r1, r1, #0x2\n\t"
     		"MSR control, r1\n\t"
 			/* Save the return value. */
@@ -95,6 +152,9 @@ int stack_switch(int (*thread)(void), void* new_stack_top) {
  * Main function.  Initializes the GPIO, Timers, and
  */
 int main() {
+	/* Turn off non-essential interrupts. */
+	__disable_irq();
+
 	if (sys_clk_init_72mhz() != SUCCESS) {
 		/* We failed. Stop. */
 		for (;;) {}
@@ -104,6 +164,10 @@ int main() {
 	motor_pins_init();
 	motor_timers_init();
 	motor_pwm_init();
+	systick_init();
+
+	/* Turn on interrupts */
+	__enable_irq();
 
 	/* Loop. Forever. */
 	for (;;) {
@@ -115,7 +179,6 @@ int main() {
  * Interrupt service routines.
  * Function callers setup in "startup_stm32f10x_md.s"
  */
-
 void SysTick_Handler() {
 
 }
